540cec4bbfb927c3c411c2f8124f5323a3f39f9c
Jamie Brandon <jamie@scattered-thoughts.net>
2011-07-09 13:55:08 -0600

    Basic twilio example

--- src/dial_a_stranger_resource.erl
+++ src/dial_a_stranger_resource.erl
@@ -3,11 +3,14 @@
 %% @doc Example webmachine_resource.
 
 -module(dial_a_stranger_resource).
--export([init/1, to_html/2]).
+-export([init/1, content_types_provided/2, to_xml/2]).
 
 -include_lib("webmachine/include/webmachine.hrl").
 
 init([]) -> {ok, undefined}.
 
-to_html(ReqData, State) ->
-    {"<html><body>Hello, new world</body></html>", ReqData, State}.
+content_types_provided(ReqData, Context) ->
+   {[{"text/xml", to_xml}], ReqData, Context}.
+
+to_xml(ReqData, State) ->
+    {"<Response><Say>Hello</Say></Response>", ReqData, State}.
